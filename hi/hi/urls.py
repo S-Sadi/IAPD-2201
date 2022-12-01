572a17ea2c01c7598f3191499e8a62a74571d196
Owner <sydul.cit.bd@gmail.com>
2022-12-01 12:49:38 +0600

    first commit

---
+++ hi/hi/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path
+from .import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login', views.login, name="login"),
+    path('logout/', views.logout, name="logout"),
+    path('registration/', views.registration, name="registration"),
+    path("", views.home, name="home"),
+    path("index", views.index, name="index"),
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
